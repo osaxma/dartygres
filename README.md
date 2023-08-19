040766999ec1f7c04dc9ffbbce5cea2e2a56ff0d
osaxma <46427323+osaxma@users.noreply.github.com>
2023-08-19 12:40:04 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+
+> NOTE THIS IS A PLACEHOLDER FOR NOW.
+
+This is intended to be a package for all things postgres in Dart.
---
+++ example/dartygres_example.dart
@@ -0,0 +1,6 @@
+import 'package:dartgres/dartgres.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
